4ca40df32e688b43217e3587cea07cb7d975bf4d
tehnplk <tehnplk@gmail.com>
2022-08-02 12:25:24 +0700

    tehnn

--- routes/emr.js
+++ routes/emr.js
@@ -2,14 +2,23 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../con_db');
 
-router.post('/diag/<cid>', async function (req, res, next) {
+router.get('/', async function (req, res, next) {
+
+    res.json({
+        'ok': 'ok'
+    })
+});
+
+router.post('/diag', async function (req, res, next) {
+    cid = req.body.cid
     result = await knex('diag').where({
         cid: cid
     })
     res.json(result)
 });
 
-router.post('/drug/<cid>', async function (req, res, next) {
+router.post('/drug', async function (req, res, next) {
+    cid = req.body.cid
     result = await knex('drug').where({
         cid: cid
     })
